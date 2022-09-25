8b9ee9197b9e9c6120b0fecbfaf40111f6e2bf61
Alexandra <sasha_shubina5@mail.ru>
2022-09-25 16:20:17 +0300

    api+http-запросы+фронтенд

--- config/urls.py
+++ config/urls.py
@@ -16,12 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-#from notes.api import api
+from notes.api import api
 from django.conf.urls.static import static
  
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	#path("api/", api.urls),
+	path("api/", api.urls),
 	path('', include('notes.urls', namespace="notes")),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
